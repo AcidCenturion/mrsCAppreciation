8751d48fadab631ba4f99515e72c9dd147f7b0d6
AcidCenturion <acidcenturion@gmail.com>
2022-05-15 20:11:41 -0700

    add game run code

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,23 @@
 public class Player {
+    /** This class holds the information about the player, like the player's location.
+     * The game method depend on where the player is located.
+     * This class will need to link to all the other scenes, and acts as the navigation between scenes.
+     */
+
+    //Create the objects to each class
+
+    //Store where the player is located. Default value at the starting location
+    String location = "Bedroom";
+
+    /* Create the method for the game
+     * Void method
+     * No Parameters
+     * While the game has not ended, repeatedly check where the player is. Depending on where the player is,
+     * run the interactables method from that scene.
+     * When the location is at an ending, end the while loop and check what ending was achieved, and print
+     * that ending.
+     */
+    public void game(){
+
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,11 @@
 public class Runner {
+    public static void main(String [] args){
+        /** This class runs the game code. */
+
+        /* Create a Player object to access the method to run the game */
+        Player playGame = new Player();
+
+        /* Run the method to play the game */
+        playGame.game();
+    }
 }
