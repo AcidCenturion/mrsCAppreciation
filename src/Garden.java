a96da7814e3f86e9ce507ffd02318a302b35e268
AcidCenturion <acidcenturion@gmail.com>
2022-05-20 09:58:05 -0700

    fix typo

--- src/Garden.java
+++ src/Garden.java
@@ -116,7 +116,7 @@ public class Garden {
         System.out.println();
         System.out.println("You obtained an egg");
         System.out.println();
-        System.out.println("\033[3m... Do not bite that hand that feeds you, chicken.\033[0m");
+        System.out.println("\033[3m... Do not bite the hand that feeds you, chicken.\033[0m");
         System.out.println();
         egg = true;
     }
